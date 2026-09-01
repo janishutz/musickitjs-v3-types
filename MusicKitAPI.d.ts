45644879b44f33ea8e9d6f2e0c438e3adb54e7c0
Janis Hutz <development@janishutz.com>
2026-09-01 08:53:56 +0200

    fix: API request description

--- MusicKitAPI.d.ts
+++ MusicKitAPI.d.ts
@@ -3,13 +3,13 @@ export interface MusicKitAPI {
      * This is the primary mean of accessing the Apple Music API.
      * @see https://js-cdn.music.apple.com/musickit/v3/docs/index.html?path=/docs/reference-javascript-api--page
      * @param path - The Apple Music API path to use
-     * @param params - The parameters to substitute in the path (params use Mustache syntax)
+     * @param queryParameters - URL query parameters to be appended to the request URL
      * @param options - Extra options to pass directly to the underlying fetch call.
      * @returns The data in an object. The type varies across paths.
      */
     'music': (
         path: string,
-        params?: {
+        queryParameters?: {
             [key: string]: unknown
         },
         opts?: {
